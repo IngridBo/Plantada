b1fd349ce44638a2d3e7f099880464350b499506
Ingrid Bonilla <112415934+IngridBo@users.noreply.github.com>
2023-05-31 21:33:20 -0500

    Add files via upload

---
+++ Plantada/JS/validacion.js
@@ -0,0 +1,34 @@
+
+function validateForm() 
+{
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let phone = document.getElementById("phone").value;
+    let message = document.getElementById("message").value;
+    let nameRegex = /^[a-zA-Z ]{2,30}$/;
+    let emailRegex = /^\S+@\S+\.\S+$/;
+    let phoneRegex = /^\d{10}$/;
+    let messageRegex = /^.{1,50}$/;
+
+    if (!nameRegex.test(name)) {
+        alert("Ingrese nombres y apellidos validos.");
+        return false;
+    }
+    if (!phoneRegex.test(phone)) {
+        alert("Ingrese un número de teléfono válido de 10 dígitos.");
+        return false;
+    }
+    if (!emailRegex.test(email)) {
+        alert("Ingrese una dirección de e-mail válida.");
+        return false;
+    }
+    if (message=="") {
+        alert("Ingrese un mensaje mayor a 50 caracteres");
+        return false;
+    }
+    sendEmail();
+}
+
+function sendEmail() {
+    console.log("Correo electronico enviado!");
+}
